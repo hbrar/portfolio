798456833be724e8361b3e9371c173d6e8eb0b9e
EC2 Default User <ec2-user@ip-172-31-22-235.us-west-2.compute.internal>
2018-01-16 05:19:46 +0000

    fixed server error: renamed temlate to templates

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import SimpleHTTPServer
 import SocketServer
 
 ip = 'localhost' # Or '127.0.0.1' instead of 'localhost'.
-port = '8081' # Or '8081' or '8082' instead of '8080'.
+port = '8080' # Or '8081' or '8082' instead of '8080'.
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 httpd = SocketServer.TCPServer((ip, int(port)), Handler)
 httpd.serve_forever()
